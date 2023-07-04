b364a0093fced2eac9c43b796e70ad7639ad60df
sachinflutter <sachingith@gmail.com>
2023-07-04 19:33:27 +0530

    second Commit

---
+++ lib/starting.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Facebook _uI/home.dart';
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home()
+    ,));
+}
